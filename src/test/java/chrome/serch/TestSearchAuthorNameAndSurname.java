2e8c0edc6bc0e76f251862d7a4ba334a02fc192e
GhostRider <kutcevolov@icloud.com>
2021-04-21 17:16:44 +0300

    add Search tests by Author name, Surname, Name and surname Fixes : 1. Deleted useless code strings in search test 2. Replaced code to 300 strings in one file (Search tests) 3. Deleted TestSearchAuthorNameAndSurnameSeparate

--- src/test/java/chrome/serch/TestSearchAuthorNameAndSurname.java
+++ src/test/java/chrome/serch/TestSearchAuthorNameAndSurname.java
@@ -8,7 +8,6 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class TestSearchAuthorNameAndSurname {
@@ -22,12 +21,10 @@ public class TestSearchAuthorNameAndSurname {
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("mark twain");
         driver.findElement(By.cssSelector("[value=Search]")).click();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        String confirmSearch = driver.getCurrentUrl();
-        assertEquals(confirmSearch, "https://www.sharelane.com/cgi-bin/search.py?keyword=mark+twain", "button " +
-                "search doesn't work");
-        String searchResult = driver.getPageSource();
-        boolean acceptableSearch = searchResult.toLowerCase().contains("mark twain");
-        assertTrue(acceptableSearch, "search result doesn't work correct");
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("mark twain"), "Search result doesn't work correct");
         driver.close();
     }
 
@@ -41,12 +38,10 @@ public class TestSearchAuthorNameAndSurname {
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("MARK TWAIN");
         driver.findElement(By.cssSelector("[value=Search]")).click();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        String confirmSearch = driver.getCurrentUrl();
-        assertEquals(confirmSearch, "https://www.sharelane.com/cgi-bin/search.py?keyword=MARK+TWAIN", "button " +
-                "search doesn't work");
-        String searchResult = driver.getPageSource();
-        boolean acceptableSearch = searchResult.toLowerCase().contains("mark twain");
-        assertTrue(acceptableSearch, "search result doesn't work correct");
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("mark twain"), "Search result doesn't work correct");
         driver.close();
     }
 
@@ -60,12 +55,239 @@ public class TestSearchAuthorNameAndSurname {
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("marktwain");
         driver.findElement(By.cssSelector("[value=Search]")).click();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        String confirmSearch = driver.getCurrentUrl();
-        assertEquals(confirmSearch, "https://www.sharelane.com/cgi-bin/search.py?keyword=marktwain", "button " +
-                "search doesn't work");
-        String searchResult = driver.findElement(By.cssSelector("[class=\"confirmation_message\"]")).getText();
-        assertEquals(searchResult, "Nothing is found :(", "Search result doesn't work correct");
+        boolean testResult = driver.findElement(By.cssSelector("[class=\"confirmation_message\"]")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
         driver.close();
     }
 
+    @Test
+    public void searchShouldAcceptByAuthorNameToLowerCase() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("mark");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("mark"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorNameToUpperCase() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("MARK");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("mark"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorNameByCamelCase() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("MaRk");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("mark"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorNameWithCapitalLetter() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("Mark");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("mark"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorNameWithSpace() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("  Mark  ");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("mark"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldNotAcceptByAuthorNameWithSpaceForEveryLetter() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys(" M a r k ");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("[class=\"confirmation_message\"]")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldNotAcceptByAuthorNameWithSymbols() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("M@ark@!#");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("[class=\"confirmation_message\"]")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorSurnameToLowerCase() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("twain");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("twain"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorSurnameToUpperCase() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("TWAIN");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("twain"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorSurnameByCamelCase() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("TwAiN");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("twain"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorSurnameWithCapitalLetter() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("Twain");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("twain"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldAcceptByAuthorSurnameWithSpace() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("  Twain  ");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("twain"), "Search result doesn't work correct");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldNotAcceptByAuthorSurnameWithSpaceForEveryLetter() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys(" T w a i n ");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("[class=\"confirmation_message\"]")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        driver.close();
+    }
+
+    @Test
+    public void searchShouldNotAcceptByAuthorSurnameWithSymbols() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.cssSelector("[type=text]")).sendKeys("#@!Tw@ain#");
+        driver.findElement(By.cssSelector("[value=Search]")).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        boolean testResult = driver.findElement(By.cssSelector("[class=\"confirmation_message\"]")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        driver.close();
+    }
 }
--- src/test/java/chrome/serch/TestSearchNameOfTheBook.java
+++ src/test/java/chrome/serch/TestSearchNameOfTheBook.java
@@ -8,7 +8,6 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class TestSearchNameOfTheBook {
@@ -18,14 +17,14 @@ public class TestSearchNameOfTheBook {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         WebDriver driver = new ChromeDriver(options);
-       driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
         driver.findElement(By.cssSelector("[type=text]")).sendKeys("  Great  ");
         driver.findElement(By.cssSelector("[value=Search]")).click();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        String confirmSearch = driver.getCurrentUrl();
-        assertEquals(confirmSearch, "https://www.sharelane.com/cgi-bin/search.py?keyword=++Great++", "button " +
-                "search doesn't work");
-        assertTrue(driver.getPageSource().toLowerCase().contains("great"), "search result doesn't work correct");
+        boolean testResult = driver.findElement(By.cssSelector("body > center > table > tbody > tr:nth-child(5) > td " +
+                "> table:nth-child(1)")).isDisplayed();
+        assertTrue(testResult, "Search doesn't work");
+        assertTrue(driver.getPageSource().toLowerCase().contains("great"), "Search result doesn't work correct");
         driver.close();
     }
 }
